be76336a0b9f42cb6c53a6d05f308964a8c936cb
JJuprising <371365866@qq.com>
2023-05-24 20:11:01 +0800

    ignore config.php

--- application/index/controller/Book.php
+++ application/index/controller/Book.php
@@ -2,11 +2,15 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Db;
 class Book extends Controller
 {
     public function book(){
         //         $data=Db::table('doctor')->select();
         //         $this->assign("result",$data);
+        //获取病人数据
+        $data = Db::table('patient')->where('patNo',session('No'))->find();
+        $this->assign('patInfo',$data);
         return $this->fetch();//执行完此方法后返回到视图view
     }
 }
