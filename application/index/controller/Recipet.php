695f9d82380cb10bd99adb659abcf17c10a13fdd
szlx560223 <764345063@qq.com>
2023-05-25 21:30:51 +0800

    处方页面

--- application/index/controller/Recipet.php
+++ application/index/controller/Recipet.php
@@ -2,11 +2,12 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Session;
 class Recipet extends Controller
 {
+    
     public function recipet(){
-        //         $data=Db::table('doctor')->select();
-        //         $this->assign("result",$data);
+        $this->getRecipetNo();
         $this->getPatInfo();
         $this->getMedicineInfo();
         return $this->fetch();//执行完此方法后返回到视图view
@@ -15,10 +16,20 @@ class Recipet extends Controller
         $patNo = $_GET['patNo'];
         if(!empty($patNo)){
             $rs = db('patient')->where('patNo',$patNo)->find();
-            if(!empty($patNo)){
+            session('patNo',$patNo);
+            if(!empty($rs)){
                 $this->assign('patInfo',$rs);
             }
         }
+    }
+    public function getRecipetNo(){
+        $max = db('recipe')->order('recipeNo','desc')->find();
+        if(empty($max)){
+            $max = '1000000';
+        }else{
+            $max = $max['recipeNo']+1;
+        }
+        session('recipeNo',$max);
     }
     public function getMedicineInfo(){
         $rs = db('medicine')->select();
@@ -27,14 +38,41 @@ class Recipet extends Controller
         }
     }
     
-    public function addCart(){
-
-        $id = input('post.No');
-        $this->recipet();
-    }
-    
-    public function dealMedicList($data){
-        return $data;
+    public function dealRecipe($medicineList,$symptom){ 
+        
+        if(Session::get('job')=='doctor'){
+            $docNo = Session::get('No');
+        }
+        $patNo = Session::get('patNo');
+        $recipeNo = Session::get('recipeNo');
+        if(empty($docNo)||empty($patNo)||empty($recipeNo)){
+            return 'dataMissing';
+        }
+         $medicineList = json_decode($medicineList,true);
+         $money = 0;
+          foreach ($medicineList as $key =>$value){
+             $medicine = db('medicine')->where('medicineNo',$key)->find();
+             if(!empty($medicine)){
+                 $money+=$medicine['price']*$value;
+             }
+             $data = [
+                    'recipeNo' => $recipeNo,
+                    'medicineNo' => $key,
+                    'medQuantity' => $value
+                    ];
+             db('medicationlist')->insert($data);
+         }
+         
+         $data = [
+                'recipeNo' => $recipeNo,
+                'symptom' => $symptom,
+                'money' => $money,
+                'docNo' => $docNo,
+                'patNo' => $patNo
+                 ];
+         
+         db('recipe')->insert($data);
+/*          return 'success'; */   
     }
 }
 
