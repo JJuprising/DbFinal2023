a8d796e2597c4583c8f6a33ecdaac2a624d55e9f
JJuprising <371365866@qq.com>
2023-05-25 22:26:47 +0800

    修改挂号取消按钮的漏洞

--- application/index/controller/Book.php
+++ application/index/controller/Book.php
@@ -19,10 +19,21 @@ class Book extends Controller
     public function postbook(){
         //提交挂号信息
 //        echo input('post.bookDate'),input('post.doctor'),session('No');
+        if(empty(input('post.bookDate'))){
+            $this->error('挂号日期不能为空');
+        }
+        if(empty(input('post.doctor'))){
+            $this->error('接诊医生不能为空');
+        }
         $result = Db::execute("INSERT INTO bookingform (bookingNo, bookingTime, docNo, patNo) VALUES(?,?,?,?)"
-            ,[session('No'),input('post.bookDate'),input('post.doctor'),session('No')]);
+            ,[null,input('post.bookDate'),input('post.doctor'),session('No')]);
     
         $this->success('挂号成功', url('book/book'));
+    }
+    public function cancel(){
+        //点击取消按钮
+        //跳转到主页
+        $this->redirect(url('index/index'));
     }
 }
 
