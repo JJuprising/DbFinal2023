999b9f94243328561bc58ca3f5e1bcbdbeb69773
jybuchiyu <2786087416@qq.com>
2023-06-17 18:53:54 +0800

    结束了

--- application/config.php
+++ application/config.php
@@ -144,7 +144,7 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__ROOT__'    =>'/dbFinal2023',
+        '__ROOT__'    =>'/DbFinal2023',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
