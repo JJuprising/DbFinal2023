f74d7d267badc80b869cbf58d0c764e23761d2cf
jybuchiyu <2786087416@qq.com>
2023-06-17 21:40:57 +0800

    ∠一下

--- runtime/temp/5402c17fb95824722cde9601b99ff38b.php
+++ runtime/temp/5402c17fb95824722cde9601b99ff38b.php
@@ -1,23 +1,18 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:3:{s:66:"D:\AppServ\www\DbFinal2023/application/index\view\index\index.html";i:1684924601;s:24:"./public/common/top.html";i:1684912657;s:27:"./public/common/bottom.html";i:1684767517;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:3:{s:66:"D:\AppServ\www\DbFinal2023/application/index\view\index\index.html";i:1684930085;s:24:"./public/common/top.html";i:1685276201;s:27:"./public/common/bottom.html";i:1685260531;}*/ ?>
 <meta charset="utf-8">
 <center>
 <table width=999px border=0>
 <tr>
-	<td rowspan=2><img src="/dbFinal2023/public/static/pic/logo.jpg" style="width:250px; height:68px"> </td>
-	<td style="font-size:24px; float:right; font-family:微软雅黑"><?php echo \think\Session::get('No'); ?>欢迎使用医院管理系统</td>
-	<td style="text-align:right;"><img src="/dbFinal2023/public/static/image/ttel.jpg"></td>
-	<!-- <td style="background:url('/dbFinal2023/public/static/image/topxmenubg.jpg'); font-size:x-small;text-align:center;">
-	<a href="member.php" style="text-decoration:none;">我的帐户</a>|
-	<a href="<?php echo url('order/showorder'); ?>" style="text-decoration:none;">我的订单</a>|
-	<a href="<?php echo url('cart/index'); ?>" style="text-decoration:none;">购物车</a>|
-	</td> -->
+<td rowspan=2><img src="/DbFinal2023/public/static/pic/logo.jpg" style="width:250px; height:68px"> </td>
+<td style="font-size:24px; float:right; font-family:微软雅黑"><?php echo \think\Session::get('No'); ?>欢迎使用医院管理系统</td>
+<td style="text-align:right;"><img src="/DbFinal2023/public/static/image/ttel.jpg"></td>
 </tr>
 
 <tr>
 	<td></td>
 	<td style="float:right;">
 	<?php if(\think\Session::get('No') == ''): ?>
-	<a href="<?php echo url('login/login'); ?>" style="font-size:16px;text-decoration:none;">登录</a>&nbsp;&nbsp;
+	<a href="<?php echo url('login/login'); ?>" style="font-size:16px;;text-decoration:none;">登录</a>&nbsp;&nbsp;
 	<?php else: ?>
 	<a href="<?php echo url('login/logOut'); ?>" style="font-size:16px;text-decoration:none;">退出</a>&nbsp;&nbsp;
 	<?php endif; ?>
@@ -26,16 +21,16 @@
 </tr>
 
 <tr><td colspan=3 style="background-color:#1E90FF; font-size: small;text-align:center; width:999px;height:40px">
- <span style=" color:White;"> |</span> &nbsp; &nbsp; &nbsp;
+ <span style="color:White;">|</span> &nbsp; &nbsp; &nbsp;
  	<!-- 医生导航栏 -->
  	<?php if(\think\Session::get('job') == 'doctor'): ?>
- 	<a href="<?php echo url('index/index'); ?>" style="text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑"> 主页</span></a>
+ 	<a href="<?php echo url('index/index'); ?>" style="text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑;"> 主页</span></a>
     &nbsp;&nbsp;&nbsp; <span style=" color:White;"> |</span> &nbsp;&nbsp;&nbsp;
-    <a href="<?php echo url('schedule/schedule'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑">排班</span></a>
+    <a href="<?php echo url('schedule/schedule'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑;">排班</span></a>
      &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
-    <a href="<?php echo url('treat/treat'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑">接诊</span></a>
+    <a href="<?php echo url('treat/treat'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑;">接诊</span></a>
      &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
-	<a href="<?php echo url('recipet/recipet'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑">处方</span></a>
+	<a href="<?php echo url('departtreat/departtreat'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑;">住院</span></a>
      &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
  	<?php endif; ?>
  	<!-- 病人导航栏 -->
@@ -48,11 +43,9 @@
      &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
     <a href="<?php echo url('pay/pay'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑">缴费</span></a>
      &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
-     <a href="<?php echo url('consult/consult'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑">查询</span></a>
-     &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
  	<?php endif; ?>
  	<!-- 管理员导航栏 -->
- 	<?php if(\think\Session::get('job') == 'administrator'): endif; if(\think\Session::get('job') == ''): ?>
+ 	<?php if(\think\Session::get('job') == 'manage'): endif; if(\think\Session::get('job') == ''): ?>
  	 	<a href="<?php echo url('index/index'); ?>" style="text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑"> 主页</span></a>
     &nbsp;&nbsp;&nbsp; <span style=" color:White;"> |</span> &nbsp;&nbsp;&nbsp;
  	<?php endif; ?>
@@ -62,7 +55,7 @@
 <html>
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
-	<link href="/dbFinal2023/public/static/css/index.css" rel="stylesheet" type="text/css">
+	<link href="/DbFinal2023/public/static/css/index.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <center>
@@ -108,11 +101,11 @@
 		</ul>
 	</div>
     <div id="img">
-			<img src="/dbFinal2023/public/static/image/hospital.jpg">
+			<img src="/DbFinal2023/public/static/image/hospital.jpg">
     </div>
 </div>
-	<div id="footer">
-Copyright © 2016, flowersforjx.com, All Rights Reserved 粤华师备 20132003040号 粤ICP备 123456号
+	<div id="footer" style="width:100%;border-top:1px solid #ccc;padding:5px;">
+Copyright © 2023, hospitalcn.com, All Rights Reserved 粤华师备 23102013039号 粤ICP备 123456号
 </div>
 </center>
 </body>
--- runtime/temp/b30b96d742fda673c3036f4659fd5815.php
+++ runtime/temp/b30b96d742fda673c3036f4659fd5815.php
@@ -1,23 +1,18 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:66:"D:\AppServ\www\DbFinal2023/application/index\view\login\login.html";i:1684773795;s:24:"./public/common/top.html";i:1684903389;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:66:"D:\AppServ\www\DbFinal2023/application/index\view\login\login.html";i:1685276201;s:24:"./public/common/top.html";i:1685276201;}*/ ?>
 <meta charset="utf-8">
 <center>
 <table width=999px border=0>
 <tr>
-	<td rowspan=2><img src="/dbFinal2023/public/static/pic/logo.jpg" style="width:250px; height:68px"> </td>
-	<td style="font-size:24px; float:right; font-family:微软雅黑"><?php echo \think\Session::get('No'); ?>欢迎使用医院管理系统</td>
-	<td style="text-align:right;"><img src="/dbFinal2023/public/static/image/ttel.jpg"></td>
-	<!-- <td style="background:url('/dbFinal2023/public/static/image/topxmenubg.jpg'); font-size:x-small;text-align:center;">
-	<a href="member.php" style="text-decoration:none;">我的帐户</a>|
-	<a href="<?php echo url('order/showorder'); ?>" style="text-decoration:none;">我的订单</a>|
-	<a href="<?php echo url('cart/index'); ?>" style="text-decoration:none;">购物车</a>|
-	</td> -->
+<td rowspan=2><img src="/DbFinal2023/public/static/pic/logo.jpg" style="width:250px; height:68px"> </td>
+<td style="font-size:24px; float:right; font-family:微软雅黑"><?php echo \think\Session::get('No'); ?>欢迎使用医院管理系统</td>
+<td style="text-align:right;"><img src="/DbFinal2023/public/static/image/ttel.jpg"></td>
 </tr>
 
 <tr>
 	<td></td>
 	<td style="float:right;">
 	<?php if(\think\Session::get('No') == ''): ?>
-	<a href="<?php echo url('login/login'); ?>" style="font-size:16px;text-decoration:none;">登录</a>&nbsp;&nbsp;
+	<a href="<?php echo url('login/login'); ?>" style="font-size:16px;;text-decoration:none;">登录</a>&nbsp;&nbsp;
 	<?php else: ?>
 	<a href="<?php echo url('login/logOut'); ?>" style="font-size:16px;text-decoration:none;">退出</a>&nbsp;&nbsp;
 	<?php endif; ?>
@@ -26,16 +21,16 @@
 </tr>
 
 <tr><td colspan=3 style="background-color:#1E90FF; font-size: small;text-align:center; width:999px;height:40px">
- <span style=" color:White;"> |</span> &nbsp; &nbsp; &nbsp;
+ <span style="color:White;">|</span> &nbsp; &nbsp; &nbsp;
  	<!-- 医生导航栏 -->
  	<?php if(\think\Session::get('job') == 'doctor'): ?>
- 	<a href="<?php echo url('index/index'); ?>" style="text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑"> 主页</span></a>
+ 	<a href="<?php echo url('index/index'); ?>" style="text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑;"> 主页</span></a>
     &nbsp;&nbsp;&nbsp; <span style=" color:White;"> |</span> &nbsp;&nbsp;&nbsp;
-    <a href="<?php echo url('schedule/schedule'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑">排班</span></a>
+    <a href="<?php echo url('schedule/schedule'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑;">排班</span></a>
      &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
-    <a href="<?php echo url('treat/treat'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑">接诊</span></a>
+    <a href="<?php echo url('treat/treat'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑;">接诊</span></a>
      &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
-	<a href="<?php echo url('recipet/recipet'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:宋体">处方</span></a>
+	<a href="<?php echo url('departtreat/departtreat'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑;">住院</span></a>
      &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
  	<?php endif; ?>
  	<!-- 病人导航栏 -->
@@ -48,11 +43,9 @@
      &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
     <a href="<?php echo url('pay/pay'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑">缴费</span></a>
      &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
-     <a href="<?php echo url('consult/consult'); ?>"  style=" text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑">查询</span></a>
-     &nbsp; &nbsp; &nbsp; <span style=" color:White;"> |</span>&nbsp;&nbsp;&nbsp;
  	<?php endif; ?>
  	<!-- 管理员导航栏 -->
- 	<?php if(\think\Session::get('job') == 'administrator'): endif; if(\think\Session::get('job') == ''): ?>
+ 	<?php if(\think\Session::get('job') == 'manage'): endif; if(\think\Session::get('job') == ''): ?>
  	 	<a href="<?php echo url('index/index'); ?>" style="text-decoration:none;"><span style=" color:White; font-weight:bold; font-size:medium; font-family:微软雅黑"> 主页</span></a>
     &nbsp;&nbsp;&nbsp; <span style=" color:White;"> |</span> &nbsp;&nbsp;&nbsp;
  	<?php endif; ?>
@@ -70,28 +63,30 @@
 
 <!-- action关联了dologin方法 -->
 <form action="<?php echo url('login/dologin'); ?>" method="post" name=f>
-<table style="width:480px; border-width:3px; border-style:dotted;">
+<table style="width:480px; border-width:3px; border-style:dotted;margin-top:40px;">
 <tr><td colspan=2 style="text-align:center;height:60px;">请登录医院管理信息系统</td></tr>
 <tr>
- <td style="text-align:right;height:30px;"><label id="NoLabel">病案号：</label></td>
-<td align=left><input type=text name="No" id="No" placeholder = "请输入病人病案号"></td>
+	<td style="text-align:left;height:30px;"><label id="NoLabel" style="margin-left:60px;">病案号：</label></td>
+	<td align=left><input type=text name="No" id="No" placeholder = "请输入病人病案号" style="width:200px; float:right; margin-right:60px;"></td>
 </tr>
 <tr>
-<td style="text-align:right;height:30px;">密码：</td>
-<td align=left><input type=password name="Password"></td>
+	<td style="text-align:left;height:30px;"><label id="psw" style="margin-left:60px;">密码：</label></td>
+	<td align=left><input type=password name="Password" style="width:200px; float:right; margin-right:60px;"></td>
 </tr>
 <tr>
-<td style="text-align:right;height:30px;">身份：</td>
-<td style="">
-<input type="radio" id="doctor" name="job" value="doctor"><label for="doctor" onclick="showNo('doctor')">医生</label>
-<input type="radio" id="patient" name="job" value="patient"  checked onclick="showNo('patient')"><label for="patient">病人</label>
-<input type="radio" id="administrator" name="job" value="administrator" onclick="showNo('administrator')"><label for="administrator">管理员</label>
-</td>
+	<td style="text-align:left;height:30px;"><label id="iden" style="margin-left:60px;">身份：</label></td>
+	<td align=left style="float:center;">
+	<div style="float:right;margin-right:60px;">
+	<input type="radio" id="doctor" name="job" value="doctor"><label for="doctor" onclick="showNo('doctor')">医生</label>
+	<input type="radio" id="patient" name="job" value="patient"  checked onclick="showNo('patient')"><label for="patient">病人</label>
+	<input type="radio" id="manage" name="job" value="manage" onclick="showNo('manage')"><label for="manage">管理员</label>
+	</div>
+	</td>
 
 </tr>
 
 <tr><td colspan=2 style="text-align:center;height:60px;">
-<input type=submit name=s value="登录">
+<input type=submit name=s value="登录" style="width:120px;height:40px;color:#fff;background-color:#1E90FF;border:none;border-radius:10px;">
 </td></tr>
 </table>
 </form>
@@ -111,7 +106,7 @@ function showNo(jobType) {
   } else if (jobType == "patient") {
     NoLabel.innerHTML = "病案号：";
     document.getElementById("No").placeholder = "请输入病人病案号";
-  } else if (jobType == "administrator") {
+  } else if (jobType == "manage") {
     NoLabel.innerHTML = "管理员编号：";
     document.getElementById("No").placeholder = "请输入管理员编号";
   }
