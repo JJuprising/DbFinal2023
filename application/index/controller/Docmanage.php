70656a6ad48363a3a9eeafa722dd9cd5217a602b
Muya-123 <ray914173732@outlook.com>
2023-06-18 00:16:02 +0800

    管理员页面

---
+++ application/index/controller/Docmanage.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+class Docmanage extends Controller
+{
+    public function Docmanage()
+    {
+        return $this->fetch();
+    }
+}
+
